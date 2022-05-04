031abd1733137d4556ea7f66fbe053a1c6a9fc41
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-04 15:10:32 -0700

    Added delete route to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,6 +107,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that ID!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

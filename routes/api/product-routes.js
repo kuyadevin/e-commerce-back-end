8fee383104d434874b69e77cae5aa2ae6d714906
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-03 17:49:42 -0700

    add get route to get all products on product routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category}, { model: Tag}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+    }
 });
 
 // get one product

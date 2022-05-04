91ed679a32d1118df17d72dc8b3c16a2592b10b3
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-04 15:29:24 -0700

    Added gitignore and delete route for tag.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,6 +47,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that ID!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;

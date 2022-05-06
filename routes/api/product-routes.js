8c1980a57f3b0f101aea94bed676d71e91c9a904
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-05 22:17:40 -0700

    Fixed product routes page

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,16 +41,10 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */ 
- await Product.create({
-   product_name: req.body.product_name,
-   price: req.body.price,
-   stock: req.body.stock,
-   category_id:req.body.category_id,
-   tagsIds: req.body.tagsIds
- })
+ await Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
